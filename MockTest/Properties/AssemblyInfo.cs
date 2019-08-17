99907abf5259e1e6fd7dbab78ac4e9e9d434510d
Cedric <cedric.michel@outlook.be>
2019-08-17 17:18:35 +0200

    add unit test

---
+++ MockTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MockTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MockTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4733bf74-a733-42b1-94b5-3136b23d2eee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MockTest/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace MockTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private List<int> _data;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _data = new List<int>() { 1, 2, 3 };
+        }
+
+        [TestMethod]
+        public void WaitingNullList()
+        {
+
+            var serviceMock = new Mock<IMeteringDataServiceAgent>();
+
+            var result = serviceMock.Object.GetDataList();
+            //Normal case because mock not defined (null by default) and return a List
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void WaitingNullIEnumerable()
+        {
+            var serviceMock = new Mock<IMeteringDataServiceAgent>();
+            var result = serviceMock.Object.GetData();
+
+            //Expected null because the mock it's not defined but it return an empty list
+            //Maybe because the method return an IEnumerable
+            Assert.IsNull(result);
+
+        }
+
+        [TestMethod]
+        public void DateTimeOffset()
+        {
+            var serviceMock = new Mock<IMeteringDataServiceAgent>();
+            serviceMock.Setup(s => s.GetDataList(It.IsAny<DateTimeOffset>())).Returns(_data);
+            var result = serviceMock.Object.GetDataList(DateTime.Now);
+
+            //Normal case
+            Assert.AreEqual(_data, result);
+
+        }
+
+        [TestMethod]
+        public void DateTimeNotWorking()
+        {
+            var serviceMock = new Mock<IMeteringDataServiceAgent>();
+            serviceMock.Setup(s => s.GetDataList(It.IsAny<DateTime>())).Returns(_data);
+            var result3 = serviceMock.Object.GetDataList(DateTime.Now);
+
+            //Expected the list but received null
+            //Because during the setup I put It.IsAny<DateTime>() and not It.IsAny<DateTimeOffset>()
+            Assert.AreEqual(_data, result3);
+
+        }
+    }
+
+    public interface IMeteringDataServiceAgent
+    {
+        IEnumerable<int> GetData();
+        List<int> GetDataList();
+        List<int> GetDataList(DateTimeOffset date);
+    }
+
+
+}
